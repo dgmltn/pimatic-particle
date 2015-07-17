47711a1e2cc4e14f24cdd53ed5656b7172ab177c
Doug Melton <doug@mobiata.com>
2015-07-16 17:52:01 -0700

    Added EventStream tests to test.js

--- test.js
+++ test.js
@@ -16,13 +16,33 @@ var token = config.token;
 var coreid = config.coreid;
 var variable = 'tempf';
 
-spark.login({accessToken: token})
-.then(function(token) {
+// Test login
+var login = spark.login({accessToken: token});
+login.then(function(token) {
   console.log('Logged in: ', token);
+}, function(err) {
+  console.log('spark error:', err);
+});
+
+// Test getVariable
+login.then(function(token) {
+  console.log("Testing getVariable...");
   return spark.getVariable(coreid, variable);
 })
 .then(function(data) {
-  console.log(variable + ' retrieved successfully: ' + data.result);
+  console.log(variable + ' retrieved successfully: ', data.result);
+}, function(err) {
+  console.log('spark error:', err);
+});
+
+// Test getEventStream
+var eventStreamHandler = function(data) {
+  console.log("Event: ", data);
+};
+
+login.then(function(token) {
+  console.log("Testing getEventStream...");
+  spark.getEventStream(false, config.coreid, eventStreamHandler);
 }, function(err) {
   console.log('spark error:', err);
 });
